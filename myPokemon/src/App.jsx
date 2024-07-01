a5c16883847386dbe4165358d7790f2ec737c16e
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-07-01 17:26:24 +0900

    update Initial design

--- myPokemon/src/App.jsx
+++ myPokemon/src/App.jsx
@@ -1,11 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import PokeList from './components/PokeList'
+import PokeDetails from './components/PokeDetails'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import MyPokedex from './components/MyPokedex'
+
+
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes >
+          <Route path='/' element={<PokeList />} />
+          <Route path='/pokemon/:id' element={<PokeDetails />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/mypokedex' element={<MyPokedex />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
